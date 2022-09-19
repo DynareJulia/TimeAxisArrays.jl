61f78e0ab9ebb919609d23260d7622ef8c377a77
MichelJuillard <michel.juillard@mjui.fr>
2022-09-19 20:27:10 +0200

    WIP

--- src/TimeAxisArrays.jl
+++ src/TimeAxisArrays.jl
@@ -1,40 +1,185 @@
 module TimeAxisArrays
 import IntervalSets: ClosedInterval
 using AxisArrays
+using ElasticArrays
 using ExtendedDates
 using PrettyTables
+using Tables
 
 export TimeAxisArray, .., diff, lag, lead
 
 const Instant = ExtendedDates.Dates.Instant
 
-struct TimeAxisArray
+abstract type AbstractTimeAxisArray{T} <: AbstractArray{T, 2} end
+    
+struct TimeAxisArray{T}  <: AbstractTimeAxisArray{T}  
     data::AxisArray
 end
 
-function TimeAxisArray(data_arg::AbstractMatrix,
-                       time::AbstractVector,
-                       variables::AbstractVector{Symbol},
-                       )
-    return TimeAxisArray(AxisArray(data_arg, time=time, variables=variables)) 
+mutable struct MTimeAxisArray{T} <: AbstractTimeAxisArray{T}  
+    data::AxisArray
 end
 
-get_axisarray(TA::TimeAxisArray) = getfield(TA, :data)
+function TimeAxisArray(data_arg::AbstractMatrix{T},
+                       time::AbstractVector,
+                       variables::AbstractVector{Symbol},
+                       ) where T
+    return TimeAxisArray{T}(AxisArray(data_arg, time=time, variables=variables)) 
+end
 
-Base.broadcastable(TA::TimeAxisArray) = TA
-struct TimeAxisArrayStyle <: Base.Broadcast.BroadcastStyle end
+function add_column!(tdf::TimeAxisArray, label, data)
+    append!(tdf.data.data, data)
+    push!(tdf.data.axes[2].val, label)
+tdf
+end
 
-Base.Broadcast.BroadcastStyle(::Type{<:TimeAxisArray}) =
-    TimeAxisArrayStyle()
+Tables.istable(TA::AbstractTimeAxisArray) = true
+Tables.schema(TA::AbstractTimeAxisArray) = Tables.Schema(vcat("Periods", TA.data.axes[2].val),
+                                                         vcat(typeof(TA.data.axes[1]),
+                                                              fill(eltype(typeof(TA.data)),
+                                                                   length(TA.data.axes[2].val))))
+Tables.columnaccess(::Type{<:AbstractTimeAxisArray}) = true
+Tables.columns(TA::AbstractTimeAxisArray) = TA
+# required AbstractColumns object methods
+Tables.getcolumn(TA::AbstractTimeAxisArray, ::Type{T}, col::Int, nm::Symbol) where {T} = (col == 1) ? format.(TA.data.axes[1].val) : TA.data.data[:, col]
+Tables.getcolumn(TA::AbstractTimeAxisArray, nm::Symbol) = (nm == :Periods) ? format.(TA.data.axes[1].val) : TA.data.data[:, nm]
+Tables.getcolumn(TA::AbstractTimeAxisArray, i::Int) = (i == 1) ? format.(TA.data.axes[1].val) : TA.data.data[:, i]
+function Tables.columnnames(TA::AbstractTimeAxisArray)
+    @show TA.data
+    return vcat(:Periods, TA.data.axes[2].val)    
+end
 
-Base.Broadcast.BroadcastStyle(::TimeAxisArrayStyle, ::Base.Broadcast.BroadcastStyle) = TimeAxisArrayStyle()
-Base.Broadcast.BroadcastStyle(::Base.Broadcast.BroadcastStyle, ::TimeAxisArrayStyle) = TimeAxisArrayStyle()
-Base.Broadcast.BroadcastStyle(::TimeAxisArrayStyle, ::TimeAxisArrayStyle) = TimeAxisArrayStyle()
+# declare that any AbstractTimeAxisArray defines its own `rows` method
+rowaccess(::Type{<:AbstractTimeAxisArray}) = true
+# just return itself, which means AbstractTimeAxisArray must iterate `AbstractRow`-compatible objects
+rows(TA::AbstractTimeAxisArray) = TA
+# a custom row type; acts as a "view" into a row of an AbstractVecOrMat
+struct MatrixRow <: Tables.AbstractRow
+    row::Int
+    source::AbstractTimeAxisArray
+end
+# the iteration interface, at a minimum, requires `eltype`, `length`, and `iterate`
+# for `AbstractTimeAxisArray` `eltype`, we're going to provide a custom row type
+Base.eltype(TA::AbstractTimeAxisArray{T}) where T = T
+Base.length(TA::AbstractTimeAxisArray) = size(TA.data, 1)
 
-import Base: getproperty, getindex, length, setindex!, show, size, diff
+Base.iterate(TA::AbstractTimeAxisArray, st=1) = st > length(TA) ? nothing : (MatrixRow(st, TA), st + 1)
 
-function getproperty(TA::TimeAxisArray, s::Symbol)
-    return TimeAxisArray(getindex(get_axisarray(TA), :, [s]))
+# required `AbstractRow` interface methods (same as for `AbstractColumns` object before)
+# but this time, on our custom row type
+getcolumn(m::MatrixRow, ::Type, col::Int, nm::Symbol) =
+    getfield(getfield(m, :source), :data)[getfield(m, :row), col]
+getcolumn(m::MatrixRow, i::Int) =
+    getfield(getfield(m, :source), :data)[getfield(m, :row), i]
+getcolumn(m::MatrixRow, nm::Symbol) =
+    getfield(getfield(m, :source), :data)[getfield(m, :row), getfield(getfield(m, :source), :lookup)[nm]]
+columnnames(m::MatrixRow) = names(getfield(m, :source))
+
+# Broadcasting
+Base.broadcastable(TA::AbstractTimeAxisArray) = TA
+
+struct AbstractTimeAxisArrayStyle <: Base.Broadcast.BroadcastStyle end
+
+Base.Broadcast.BroadcastStyle(::Type{<:AbstractTimeAxisArray}) =
+    AbstractTimeAxisArrayStyle()
+
+Base.Broadcast.BroadcastStyle(::AbstractTimeAxisArrayStyle, ::Base.Broadcast.BroadcastStyle) = AbstractTimeAxisArrayStyle()
+Base.Broadcast.BroadcastStyle(::Base.Broadcast.BroadcastStyle, ::AbstractTimeAxisArrayStyle) = AbstractTimeAxisArrayStyle()
+Base.Broadcast.BroadcastStyle(::AbstractTimeAxisArrayStyle, ::AbstractTimeAxisArrayStyle) = AbstreactTimeAxisArrayStyle()
+
+function copyto_widen!(res::AbstractVector{T}, bc::Base.Broadcast.Broadcasted,
+                       pos, col) where T
+    for i in pos:length(Base.axes(bc)[1])
+        val = bc[CartesianIndex(i, col)]
+        S = typeof(val)
+        if S <: T || promote_type(S, T) <: T
+            res[i] = val
+        else
+            newres = similar(Vector{promote_type(S, T)}, length(res))
+            copyto!(newres, 1, res, 1, i-1)
+            newres[i] = val
+            return copyto_widen!(newres, bc, i + 1, col)
+        end
+    end
+    return res
+end
+
+function getcolbc(bcf::Base.Broadcast.Broadcasted{Style}, colind) where {Style}
+    # we assume that bcf is already flattened and unaliased
+    newargs = map(bcf.args) do x
+        Base.Broadcast.extrude(x isa AbstractTimeAxisArray ? x[:, colind] : x)
+    end
+    Base.Broadcast.Broadcasted{Style}(bcf.f, newargs, bcf.axes)
+end
+
+function Base.copy(bc::Base.Broadcast.Broadcasted{AbstractTimeAxisArrayStyle})
+    ndim = length(Base.axes(bc))
+    if ndim != 2
+        throw(DimensionMismatch("cannot broadcast an AbstractTimeAxisArrayStyle into $ndim dimensions"))
+    end
+
+    
+    bcf = Base.Broadcast.flatten(bc)
+    first_ta = true
+    local periods, continuous
+    colnames = []
+    for ta in bcf.args
+        if ta isa AbstractTimeAxisArray
+            if first_ta
+                periods = getfield(ta.data, :axes)[1].val
+                first_ta = false
+            elseif getfield(ta.data, :axes)[1].val != periods
+                error("TimeAxisArrays don't have the same periods")
+            end
+        end
+    end
+    colnames = unique!([ta.data.axes[2].val for ta in bcf.args if ta isa AbstractTimeAxisArray])
+    if length(colnames) > 1 && any(length(c) != 1 for c in colnames)
+        wrongnames = setdiff(union(colnames...), intersect(colnames...))
+        if isempty(wrongnames)
+            throw(ArgumentError("Column names in broadcasted AbstractTimeAxisArray " *
+                                "must have the same order"))
+        else
+            msg = join(wrongnames, ", ", " and ")
+            throw(ArgumentError("Column names in broadcasted AbstractTimeAxisArray must match. " *
+                                "Non matching column names are $msg"))
+        end
+    end
+    nrows = length(Base.axes(bcf)[1])
+    ncols = length(colnames[1])
+    ta = TimeAxisArray{Float64}(AxisArray(Matrix(undef, nrows, ncols), periods, colnames[1]))
+    for i in Base.axes(bcf)[2]
+        if nrows == 0
+            col = Any[]
+        else
+            bcf′ = getcolbc(bcf, i)
+            v1 = bcf′[CartesianIndex(1, i)]
+            startcol = similar(Vector{typeof(v1)}, nrows)
+            startcol[1] = v1
+            col = copyto_widen!(startcol, bcf′, 2, i)
+        end
+        ta[:, colnames[1][i]] = col
+    end
+    return ta
+end
+
+function Base.copyto!(ta::AbstractTimeAxisArray, bc::Base.Broadcast.Broadcasted)
+    copyto!(ta.data, bc)
+end
+
+import Base: getproperty, getindex, length, ndims, setindex!, show, size, diff
+
+Base.ndims(::AbstractTimeAxisArray) = 2
+Base.ndims(::Type{<:AbstractTimeAxisArray}) = 2
+
+function getproperty(TA::TimeAxisArray{T}, s::Symbol) where T
+    data = getfield(TA, :data)
+    if s == :data
+        return data
+    elseif s == :axes
+        return getfield(TA.data, :axes)
+    end
+    return TimeAxisArray{T}(getindex(data, :, [s]))
 end
 
 function format(p::D) where {D <: Instant}
@@ -44,31 +189,36 @@ end
 format(p) = string(p)
 
 function Base.show(io::IO, TA::TimeAxisArray)
-    aa = get_axisarray(TA)
-    pretty_table(hcat(format.(aa.axes[1].val), aa.data), header=vcat("Period",collect(aa.axes[2])))
+    aa = TA.data
+    @show aa
+    pretty_table(aa, nosubheader=true)
 end
 
-Base.size(TA::TimeAxisArray) = size(get_axisarray(TA))
+Base.size(TA::TimeAxisArray) = size(TA.data)
 
-Base.getindex(TA::TimeAxisArray, idx::Symbol...) = TimeAxisArray(view(get_axisarray(TA), :, [idx...]))
-function Base.getindex(TA::TimeAxisArray, idx::D) where D <: Instant
-    aa = get_axisarray(TA)
+Base.getindex(TA::TimeAxisArray{T}, idx::Symbol...) where T = TimeAxisArray{T}(view(TA.data, :, [idx...]))
+function Base.getindex(TA::TimeAxisArray{T}, idx::D) where {T, D <: Instant}
+    aa = TA.data
     return TimeAxisArray(reshape(view(aa, idx, :), 1, size(aa, 2)),
                          [idx],
                          aa.axes[2].val)
 end
-Base.getindex(TA::TimeAxisArray, idx::ClosedInterval{<: Instant}) = TimeAxisArray(view(get_axisarray(TA), idx, :))
-Base.getindex(TA::TimeAxisArray, idx1::D, idx2::Symbol) where D <: Instant = TimeAxisArray(hcat(get_axisarray(TA)[idx1, idx2]), [idx1], [idx2])
-Base.getindex(TA::TimeAxisArray, idx1, idx2...) = TimeAxisArray(view(get_axisarray(TA), idx1, [idx2...]))
+Base.getindex(TA::AbstractTimeAxisArray{T}, idx::ClosedInterval{<: Instant}) where T = TimeAxisArray{T}(view(TA.data, idx, :))
+Base.getindex(TA::AbstractTimeAxisArray{T}, idx1::D, idx2::Symbol) where {T, D <: Instant} = TimeAxisArray(hcat(TA.data[idx1, idx2]), [idx1], [idx2])
+Base.getindex(TA::AbstractTimeAxisArray{T}, idx::CartesianIndex) where T = getindex(TA.data, idx)
+function Base.getindex(TA::AbstractTimeAxisArray{T}, idx1...) where T
+    @show idx1
+    return TimeAxisArray{T}(view(TA.data, idx1...))
+end
 
-function Base.setindex!(TA::TimeAxisArray, x, idx1, idx2)
-    d = get_axisarray(TA)
+function Base.setindex!(TA::TimeAxisArray{T}, x, idx1, idx2) where T
+    d = TA.data
     if !isa(idx2, Colon) && !checkvariables(d.axes[2].val, idx2)
         d = addvariables(d, idx2)
     end
     d = addperiods(d, idx1)
     setindex!(d, x, idx1, idx2)
-    return TimeAxisArray(d)
+    return TimeAxisArray{T}(d)
 end
 
 function addperiods(AA::AxisArray, idx::AbstractVector)
@@ -115,38 +265,38 @@ function addvariables(AA::AxisArray, idx)
     return AxisArray(d, time=p1:p2, variable=AA.axes[2].val)
 end
 
-function Base.diff(TA::TimeAxisArray, k::Int64=1)
-    d = get_axisarray(TA)
+function Base.diff(TA::TimeAxisArray{T}, k::Int64=1) where T
+    d = TA.data
     n, m = size(d)
     abs(k) > n - 1 && return(TimeAxisArray(repeat([missing], n, m), d.axes[1].val, d.axes[2].val))
     d1 = d[time=k+1:n]
-    return TimeAxisArray(AxisArray(vcat(repeat([missing], k, m),
-                                        d1 - d[time=1:n-k]),
-                                   time=d.axes[1].val,
-                                   variables=d.axes[2].val))
+    return TimeAxisArray{T}(AxisArray(vcat(repeat([missing], k, m),
+                                           d1 - d[time=1:n-k]),
+                                      time=d.axes[1].val,
+                                      variables=d.axes[2].val))
 end
 
-function TimeAxisArray(f::Function, TA::TimeAxisArray, args...)
-    aa = get_axisarray(TA)
+function TimeAxisArray(f::Function, TA::TimeAxisArray{T}, args...) where T
+    aa = TA.data
     f1(x) = f(x, args...)
-    return TimeAxisArray(map(f1, aa), aa.axes[1].val, aa.axes[2].val)
+    return TimeAxisArray{T}(map(f1, aa), aa.axes[1].val, aa.axes[2].val)
 end
 
-function lag(TA::TimeAxisArray, k::Int64=1)
-    d = get_axisarray(TA)
+function lag(TA::TimeAxisArray{T}, k::Int64=1) where T
+    d = TA.data
     n, m = size(d)
-    abs(k) > n - 1 && return(TimeAxisArray(repeat([missing], n, m), d.axes[1].val, d.axes[2].val))
-    return TimeAxisArray(AxisArray(vcat(repeat([missing], k, m),
+    abs(k) > n - 1 && return(TimeAxisArray{T}(repeat([missing], n, m), d.axes[1].val, d.axes[2].val))
+    return TimeAxisArray{T}(AxisArray(vcat(repeat([missing], k, m),
                                         d[time=1:n-k]),
                                    time=d.axes[1].val,
                                    variables=d.axes[2].val))
 end
 
-function lead(TA::TimeAxisArray, k::Int64=1)
-    d = get_axisarray(TA)
+function lead(TA::TimeAxisArray{T}, k::Int64=1) where T
+    d = TA.data
     n, m = size(d)
-    abs(k) > n - 1 && return(TimeAxisArray(repeat([missing], n, m), d.axes[1].val, d.axes[2].val))
-    return TimeAxisArray(AxisArray(vcat(d[time=k+1:n],
+    abs(k) > n - 1 && return(TimeAxisArray{T}(repeat([missing], n, m), d.axes[1].val, d.axes[2].val))
+    return TimeAxisArray{T}(AxisArray(vcat(d[time=k+1:n],
                                         repeat([missing], k, m)),
                                    time=d.axes[1].val,
                                    variables=d.axes[2].val))
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,19 @@ ta1 = TimeAxisArray(rand(4,3), Undated(11):Undated(14), [:a, :b, :c])
 ta2 = TimeAxisArray(rand(4,3), period(Week, 1935, 2):period(Week, 1935, 5), [:a, :b, :c])
 
 @show ta2[:b] 
-@show ta2[period(Week, 1935, 3)]
+@show ta2[WeekSE(1935, 3)]
 @show ta2[:b, :c]
-@show ta2[period(Week, 1935, 3)..period(Week, 1935, 4)]
-@show ta2[period(Week, 1935, 3)..period(Week, 1935, 4), :b, :c]
-@show ta2[period(Week, 1935, 3), :c]
+@show ta2[WeekSE(1935, 3)..WeekSE(1935, 4)]
+@show ta2[WeekSE(1935, 3)..WeekSE(1935, 4), [:b, :c]]
+@show ta2[WeekSE(1935, 3), :c]
+
+@test ta2.b.data.data ≈ ta2[:b].data
+@test ta2[:b].data ≈ ta2.data[:,:b].data
+@test ta2[WeekSE(1935, 3)].data ≈ reshape(ta2.data[2, :], 1, 3)
+@test ta2[:b, :c].data ≈ ta2.data[:, [2, 3]]
+@test ta2[WeekSE(1935, 3)..WeekSE(1935, 4)].data ≈ ta2.data[[2, 3], :]
+@test ta2[WeekSE(1935, 3)..WeekSE(1935, 4), [:b, :c]].data ≈ ta2.data[[2, 3], [2,3]]
+@test ta2[WeekSE(1935, 3), :c].data.data[1,1] == ta2.data[2, 3]
+
+@test all(log.(ta1).data ≈ log.(ta1.data))
+@test ta1 + ta1 .== 2*ta1
